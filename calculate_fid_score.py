edb5471fb11b0537d6ee4af09ba32a8fc1de6175
LeTienQuyet <21520428@gm.uit.edu.vn>
2025-08-22 20:40:53 +0700

    calculate FDI score (metric)

---
+++ calculate_fid_score.py
@@ -0,0 +1,26 @@
+from torcheval.metrics import FrechetInceptionDistance
+import torch
+import torch.nn.functional as F
+
+def calculate_fid_score(generator, dev_dataloader, batch_size=128, num_dims=100, device="cuda"):
+    fid = FrechetInceptionDistance().to(device)
+    generator.eval()
+
+    with torch.no_grad():
+        for real_images in dev_dataloader:
+            real_images = real_images.to(device)
+            fid.update(real_images, is_real=True)
+
+            # Fake image
+            noises = torch.randn(real_images.size(0), num_dims, 1, 1, device=device)
+            fake_images = generator(noises)
+
+            # Range from [-1, 1] to [0, 1]
+            fake_images = (fake_images + 1) / 2
+            fake_images = fake_images.clamp(0, 1)
+
+            # Resize 64 x 64 => 299 x 299
+            fake_images = F.interpolate(fake_images, size=(299, 299), mode="bilinear", align_corners=False)
+
+            fid.update(fake_images, is_real=False)
+    return fid.compute().item()
